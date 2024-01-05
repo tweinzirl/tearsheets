001c9fadb5916a1a570c5ec9db925c46de4e39ba
T W <tweinzirl@gmail.com>
2024-01-04 16:31:37 -0800

    bankers per branch

--- data/generic_bank/setup_bank.py
+++ data/generic_bank/setup_bank.py
@@ -16,6 +16,8 @@ Generate data for a generic bank:
 import numpy as np
 import pandas as pd
 
+import config
+
 def regions(n):
    '''Return n distinct regions and regional managers'''
    x = np.arange(1, n+1, 1)
@@ -31,11 +33,47 @@ def regions_and_branches(n_regions, n_branches):
     Todo: allow different regions to have different number of branches
     '''
     regions_ = regions(n_regions)
-    branches_ = branches(n_branches)
-    branches_ = regions_.merge(branches_, how='cross')
+    df = branches(n_branches)
+    df = regions_.merge(df, how='cross')
 
-    branches_.Branch_Number = range(1, branches_.shape[0]+1, 1)
-    branches_.Branch_Manager = range(1, branches_.shape[0]+1, 1)
+    df.Branch_Number = range(1, df.shape[0]+1, 1)
+    df.Branch_Manager = range(1, df.shape[0]+1, 1)
 
-    return branches_
+    return df
     
+def assign_personnel_to_branches(df):
+    '''
+    Calculates bankers per each branch. Returns to dataframes:
+    1. branch-level table with headcount and number of bankers per category
+    2. banker-level table with unique banker id per each banker
+    '''
+    headcount_per_branch = np.random.normal(config.HEADCOUNT_AVG,
+        config.HEADCOUNT_STD, size=df.shape[0]).astype(int)
+
+    df = df.assign(Headcount = headcount_per_branch)
+    df = df.assign(N_RM = lambda x: config.f_RM * x.Headcount)
+    df = df.assign(N_Wealth_Advisor = lambda x: config.f_Wealth_Advisor * x.Headcount)
+
+    df = df.round(0)
+
+    # make dataframe of bankers at each branch
+    banker_df = pd.DataFrame()
+    for idx, row in df.iterrows():
+        n_rm = int(row.N_RM)
+        n_wa = int(row.N_Wealth_Advisor)
+        br = row.Branch_Number
+        n = int(row.Headcount)
+
+        data_dict = {'Branch_Number': n*[br],
+                     'Banker_Type': n_rm*['RM'] + n_wa*['Wealth Advisor'],
+                     'Banker_ID': range(1,n+1,1)
+                     }
+
+        row_df = pd.DataFrame(data_dict)
+
+        banker_df = pd.concat([banker_df, row_df])
+
+    # unique banker id
+    banker_df.Banker_ID = range(1,banker_df.shape[0]+1,1)
+
+    return df.astype(int), banker_df
