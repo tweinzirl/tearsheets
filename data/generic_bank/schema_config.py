e27b1fc3fdeecd02cd7bf12160162ef2c8805064
motelian <momotelian@gmail.com>
2024-03-22 12:12:43 -0700

    add bankers and accounts table

--- data/generic_bank/schema_config.py
+++ data/generic_bank/schema_config.py
@@ -22,4 +22,36 @@ config = {
             },
             "primary_key" : "ID",
         },
+        # add ID as primary key to the accounts table
+        # remove client_type, wealth_tier from accounts table
+        # primary_banker is at client level
+        "accounts": {
+            "column_mapping": {
+                "ID": "ID",
+                "Client_ID": "Client_ID",
+                "Account_Category": "Product_Group",
+                "Account_Nr": "Acct_Nbr",
+                "Account_Type": "Product_Type",
+                "Init_Balance": "Balance",
+                "Banker_ID": "Banker_ID",
+                "Primary_Banker": "Primary_Banker",
+                "Open_Date": "Start_Dt"
+            },
+            "primary_key" : "ID",
+            "foreign_keys": [
+                {"column": "Client_ID", "references_table": "clients", "references_column": "ID"},
+                {"column": "Banker_ID", "references_table": "bankers", "references_column": "ID"},
+            ]
+        },
+        "bankers": {
+            "column_mapping": {
+                "Banker_ID": "ID",
+                "Banker_Name": "Name",
+                "Banker_Type" : "Type",
+                "Branch_Number": "Branch_Nbr",
+                "Login": "Login",
+                "Region" : "Region",
+            },
+            "primary_key" : "ID",
+        }, 
     }
